e52a2aa57633908ce696ee1bb040e869c99dd46a
Apolomir <60337783+Apolomir@users.noreply.github.com>
2021-12-05 09:21:23 +0200

    added table on ui (#1)
    create table on ui

--- resources/js/preload.js
+++ resources/js/preload.js
@@ -9,12 +9,5 @@ const exchangeData = fs.readFileSync('resources/data/exchange-data.json', 'utf-8
 contextBridge.exposeInMainWorld('exchangeData', exchangeData);
 
 window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector)
-        if (element) element.innerText = text
-    }
-
-    for (const dependency of ['chrome', 'node', 'electron']) {
-        replaceText(`${dependency}-version`, process.versions[dependency])
-    }
+    
 })
